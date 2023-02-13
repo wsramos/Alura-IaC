5a1b804d56e3ec1cc494b17d5895bbd4c048bf7d
William Ramos <contato.williamramos@outlook.com>
2023-02-13 20:45:03 -0300

    Projeto criado + Config inicial

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0557a15b87f6559cf"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "PrimeiraInstancia"
+  }
+}
