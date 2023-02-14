3542d45bde0d235b73233e50f91f14d2ec3e71b1
William Ramos <contato.williamramos@outlook.com>
2023-02-13 21:43:31 -0300

    Criando arquivos pelo terraform

--- main.tf
+++ main.tf
@@ -17,6 +17,12 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0557a15b87f6559cf"
   instance_type = "t2.micro"
   key_name = "iac-estudo"
+  user_data = <<-EOF
+                  #!/bin/bash
+                  cd /home/ubuntu
+                  echo "<h1>Feito com Terraform</h1>" > index.html
+                  nohup busybox httpd -f -p 8080 &
+                 EOF
   tags = {
     Name = "PrimeiraInstancia"
   }
